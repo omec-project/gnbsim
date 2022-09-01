de56f0333e84934e4f02d619567f2f776f3aac2e
Vini Gajjar <vini.gajjar@gslab.com>
2022-09-01 06:32:31 +0530

    fix: Remove hardcoding for User Location Information (#65)
    1) gNB will use the selected PLMN sent by Real UE
           to fetch the corresponding TAI from its configuration
        2) gNB will use the fetched TAI to populate the User
           Location Information in Uplink NGAP messages

--- gnodeb/context/gnodeb.go
+++ gnodeb/context/gnodeb.go
@@ -5,12 +5,21 @@
 package context
 
 import (
+	"encoding/hex"
+	"fmt"
+	"strconv"
+
+	"github.com/omec-project/aper"
 	transport "github.com/omec-project/gnbsim/transportcommon"
+	"github.com/omec-project/ngap/ngapConvert"
+	"github.com/omec-project/ngap/ngapType"
 
 	"github.com/omec-project/idgenerator"
 	"github.com/omec-project/openapi/models"
 	"github.com/sirupsen/logrus"
 )
+
+const MAX_CGI_BIT_LENGTH int32 = 36
 
 // GNodeB holds the context for a gNodeB. It manages the control plane and
 // user plane layer of a gNodeB.
@@ -50,6 +59,65 @@ func (gnb *GNodeB) GetDefaultAmf() *GnbAmf {
 
 func (gnb *GNodeB) AllocateRanUeNgapID() (int64, error) {
 	return gnb.RanUeNGAPIDGenerator.Allocate()
+}
+
+func (gnb *GNodeB) GetUserLocation(selectedPlmn *models.PlmnId) (*ngapType.UserLocationInformationNR, error) {
+	ueLocInfo := new(ngapType.UserLocationInformationNR)
+	ueLocInfo.NRCGI.PLMNIdentity = ngapConvert.PlmnIdToNgap(*gnb.RanId.PlmnId)
+
+	gnbId := gnb.RanId.GNbId
+
+	// CGI is formed by appending cell ID (4 - 14 bits) to gNB ID (22 - 32) bits
+	// We currently have gNB ID configured, so applying left shift operation
+	// according to its length. (The leftmost bits of the NR Cell Identity IE
+	// correspond to the gNB ID - TS 38.413)
+	gnbIdUint, err := strconv.ParseUint(gnbId.GNBValue, 16, int(gnbId.BitLength))
+	if err != nil {
+		return nil, fmt.Errorf("invalid gnbid configured:%v", err)
+	}
+	gnbIdUint = gnbIdUint << (MAX_CGI_BIT_LENGTH - gnbId.BitLength)
+	// Cell ID
+	gnbIdUint |= 0x01
+
+	cgi := fmt.Sprintf("%x", gnbIdUint)
+
+	// 36 bits sums up to odd byte count, so prepending 0 to make byte
+	// count as 40
+	cgi = "0" + cgi
+
+	bs, err := hex.DecodeString(cgi)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode hex string: %v", err)
+	}
+
+	ueLocInfo.NRCGI.NRCellIdentity.Value = aper.BitString{
+		Bytes:     bs,
+		BitLength: 36,
+	}
+
+	tai := gnb.GetTaiFromSelectedPLMN(selectedPlmn)
+	if tai == nil {
+		return nil, fmt.Errorf("plmn configured for ue is not supported by gnb. check supported ta list")
+	}
+	ueLocInfo.TAI = ngapConvert.TaiToNgap(*tai)
+	return ueLocInfo, nil
+}
+
+// GetTaiFromSelectedPLMN fetches the TAI corresponding to the selectedPlmn
+// from the Supported TA list in gNB
+func (gnb *GNodeB) GetTaiFromSelectedPLMN(selectedPlmn *models.PlmnId) *models.Tai {
+	for _, ta := range gnb.SupportedTaList {
+		for _, plmn := range ta.BroadcastPLMNList {
+			if plmn.PlmnId == *selectedPlmn {
+				tai := &models.Tai{
+					PlmnId: selectedPlmn,
+					Tac:    ta.Tac,
+				}
+				return tai
+			}
+		}
+	}
+	return nil
 }
 
 type SupportedTA struct {
