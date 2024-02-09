5168fd7fe118058b02371e5a76948e9dc4d16f74
gab-arrobo <gabriel.arrobo@intel.com>
2024-02-09 08:31:23 -0800

    Use milenage from the util repo (#138)

--- util/test/ranUe.go
+++ util/test/ranUe.go
@@ -13,11 +13,11 @@ import (
 
 	"github.com/omec-project/CommonConsumerTestData/UDR/TestRegistrationProcedure"
 	"github.com/omec-project/UeauCommon"
-	"github.com/omec-project/milenage"
 	"github.com/omec-project/nas/nasMessage"
 	"github.com/omec-project/nas/nasType"
 	"github.com/omec-project/nas/security"
 	"github.com/omec-project/openapi/models"
+	"github.com/omec-project/util/milenage"
 )
 
 type RanUeContext struct {
