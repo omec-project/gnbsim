a79417db8be08b96ffe4118168691a5e58d18bc7
Gabriel Arrobo <gabriel.arrobo@intel.com>
2025-10-23 18:32:24 -0700

    Remove github.com/yerden/go-util dependency that is only used in gnbsim repo (#376)
    * Remove github.com/yerden/go-util dependency that is only used in gnbsim repo
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    * Address Copilot's suggestions/comments
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>
    
    ---------
    
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- realue/util/util.go
+++ realue/util/util.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	"github.com/omec-project/openapi/models"
-	"github.com/yerden/go-util/bcd"
 )
 
 const (
@@ -21,6 +20,49 @@ const (
 )
 
 var ROUTING_INDICATOR []uint8 = []uint8{0xf0, 0xff}
+
+// encodeBCD encodes a string of decimal digits into telephony BCD (Binary Coded
+// Decimal) format.
+//
+// Telephony BCD encoding specifics:
+//   - Each byte encodes two digits: the second digit is placed in the upper
+//     nibble (bits 4-7), and the first digit is placed in the lower nibble
+//     (bits 0-3).
+//   - If the input string has an odd number of digits, the final byte's upper
+//     nibble is padded with 0xF.
+//   - For example, "123" encodes to [0x21, 0xF3].
+//   - Only decimal digits ('0'-'9') are allowed; any other character will
+//     result in an error.
+func encodeBCD(digits string) ([]byte, error) {
+	for _, char := range digits {
+		if char < '0' || char > '9' {
+			return nil, fmt.Errorf("invalid character '%c' in digits string", char)
+		}
+	}
+
+	digitBytes := []byte(digits)
+	length := len(digitBytes)
+
+	encodedLen := (length + 1) / 2
+	encoded := make([]byte, encodedLen)
+
+	for i := 0; i < length; i += 2 {
+		firstDigit := digitBytes[i] - '0'
+
+		var secondDigit byte
+		if i+1 < length {
+			secondDigit = digitBytes[i+1] - '0'
+		} else {
+			secondDigit = 0xF
+		}
+
+		// In telephony BCD, the second digit goes in the upper nibble
+		// and the first digit goes in the lower nibble
+		encoded[i/2] = (secondDigit << 4) | firstDigit
+	}
+
+	return encoded, nil
+}
 
 func SupiToSuci(supi string, plmnid *models.PlmnId) ([]byte, error) {
 	supiExpectedPrefix := "imsi-" + plmnid.Mcc + plmnid.Mnc
@@ -35,28 +77,24 @@ func SupiToSuci(supi string, plmnid *models.PlmnId) ([]byte, error) {
 	octet := (SUPI_FORMAT << 4) | ID_TYPE
 	suci = append(suci, octet)
 
-	enc := bcd.NewEncoder(bcd.Telephony)
-	bcdMcc := make([]byte, bcd.EncodedLen(len(plmnid.Mcc)))
-	_, err := enc.Encode(bcdMcc, []byte(plmnid.Mcc))
+	bcdMcc, err := encodeBCD(plmnid.Mcc)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode mcc in bcd format:%v", err)
+		return nil, fmt.Errorf("failed to encode mcc in bcd format: %v", err)
 	}
 	suci = append(suci, bcdMcc...)
 
-	bcdMnc := make([]byte, bcd.EncodedLen(len(plmnid.Mnc)))
-	_, err = enc.Encode(bcdMnc, []byte(plmnid.Mnc))
+	bcdMnc, err := encodeBCD(plmnid.Mnc)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode mnc in bcd format:%v", err)
+		return nil, fmt.Errorf("failed to encode mnc in bcd format: %v", err)
 	}
 	suci = append(suci, bcdMnc...)
 	suci = append(suci, ROUTING_INDICATOR...)
 	suci = append(suci, PROTECTION_SCHEME_ID)
 	suci = append(suci, PUBLIC_KEY_ID)
 
-	bcdMsin := make([]byte, bcd.EncodedLen(len(msin)))
-	_, err = enc.Encode(bcdMsin, []byte(msin))
+	bcdMsin, err := encodeBCD(msin)
 	if err != nil {
-		return nil, fmt.Errorf("failed to encode msin in bcd format:%v", err)
+		return nil, fmt.Errorf("failed to encode msin in bcd format: %v", err)
 	}
 	suci = append(suci, bcdMsin...)
 
